e717dae4085b511411cf2d9bb86f597009022ac1
arthur <arthur.gomes.4718@gmail.com>
2019-05-28 10:16:41 -0300

    firestore singleton

--- app/src/main/java/devarthur4718/com/firebaseapp/util/FireStoreUtil.kt
+++ app/src/main/java/devarthur4718/com/firebaseapp/util/FireStoreUtil.kt
@@ -1,5 +1,19 @@
 package devarthur4718.com.firebaseapp.util
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+
 //https://www.youtube.com/watch?v=eHA9jGT_87Q
 object FireStoreUtil {
+     private val firestoreInstance : FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+
+     private val currentUserDocRef : DocumentReference
+          get() = firestoreInstance.document("users/${FirebaseAuth.getInstance().uid
+               ?: throw  NullPointerException("Uid is null")}")
+
+     fun initCurrentUserIfFirstTime(){
+
+     }
+
 }
